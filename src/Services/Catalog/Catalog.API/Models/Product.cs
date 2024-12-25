b4189daa7fa628bd4a4a7e3913d7b742f5793932
Amir Makram <amakram@meemdev.com>
2024-12-25 19:14:40 +0200

    Add product and start in VSA

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public List<string> Tags { get; private set; }
+        public decimal Price { get; private set; }
+        public string Description { get; private set; }
+    }
+}
