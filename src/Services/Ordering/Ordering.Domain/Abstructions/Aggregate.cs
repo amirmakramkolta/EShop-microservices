ef5d66ebddc2975d9e7c6201720048759aeb5f6d
Amir Makram <35640764+amirmakramkolta@users.noreply.github.com>
2025-01-03 02:29:56 +0200

    Add Abstruction for DDD

---
+++ src/Services/Ordering/Ordering.Domain/Abstructions/Aggregate.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Domain.Abstructions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+        public IReadOnlyList<IDomainEvent> Events => _domainEvents.AsReadOnly();
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+        public IDomainEvent[] ClearDomainEvent()
+        {
+            IDomainEvent[] domainEvents = _domainEvents.ToArray();
+            _domainEvents.Clear();
+            return domainEvents;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Abstructions/IAggregate.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Domain.Abstructions
+{
+    public interface IAggregate<T> : IAggregate, IEntity<T>
+    {
+
+    }
+    public interface IAggregate : IEntity
+    {
+        public IReadOnlyList<IDomainEvent> Events { get; }
+        public IDomainEvent[] ClearDomainEvent();
+    }
+}
