7e6b56a05169173a77b16782b6904dfc23dc2a35
Amir Makram <35640764+amirmakramkolta@users.noreply.github.com>
2025-01-15 00:37:11 +0200

    Add some Dto and Command handler setup

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public class CreateCommandHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+    {
+        public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new CreateOrderResult(Guid.NewGuid()));
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder
+{
+    public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
+    public record CreateOrderResult(Guid Id);
+
+    public class CreateOrderValidator : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderValidator()
+        {
+            RuleFor(x=>x.Order.OrderName).NotEmpty().WithMessage("Name is required");
+            RuleFor(x=>x.Order.CustomerId).NotNull().WithMessage("Customer id is required");
+            RuleFor(x=>x.Order.OrderItems).NotEmpty().WithMessage("OrderItem should not be Empty");
+        }
+    }
+}
