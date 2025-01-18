5ad6ab2000aa4c75cd085c5e3eb280787926c135
Amir Makram <35640764+amirmakramkolta@users.noreply.github.com>
2025-01-18 22:29:38 +0200

    Add Order APIs

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -0,0 +1,27 @@
+﻿using Ordering.Application.Orders.Queries.GetOrderByName;
+
+namespace Ordering.API.Endpoints
+{
+    public record GetOrdersByNameResonse(IEnumerable<OrderDto> Orders);
+    public class GetOrdersByName : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders/{name}", async (string name, ISender sender) =>
+            {
+                var query = new GetOrdersByNameQuery(name);
+                var result = await sender.Send(query);
+
+                var response = result.Adapt<GetOrdersByNameResonse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("GetOrdersByName")
+                .Produces<GetOrdersByNameResonse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Get Orders By Name")
+                .WithDescription("Get Orders by Name");
+        }
+    }
+}
