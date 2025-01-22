a9b08005fcc352e0b9e1de5d6f8c666a8c8181e2
Amir Makram <35640764+amirmakramkolta@users.noreply.github.com>
2025-01-22 19:04:18 +0200

    Add Integration Events, feature mangment, next I will do rabbitMq config on docker compose

---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
+namespace Basket.API.Basket.CheckoutBasket
+{
+    public record CheckoutBasketCommand(CheckoutBasketDto dto) : ICommand<CheckoutBasketResult>;
+    public record CheckoutBasketResult(bool IsSuccess);
+    public class CheckoutBasketCommandValidator : AbstractValidator<CheckoutBasketCommand>
+    {
+        public CheckoutBasketCommandValidator()
+        {
+            RuleFor(x=>x.dto).NotNull().NotEmpty().WithMessage("BasketCheckoutDto is required");
+            RuleFor(x=>x.dto.UserName).NotNull().NotEmpty().WithMessage("Username  is required");
+        }
+    }
+    public class CheckoutBasketCommandHandler(IBasketIRepository repository, IPublishEndpoint publishEndpoint)
+        : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+    {
+        public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand request, CancellationToken cancellationToken)
+        {
+            var basket = await repository.GetShoppingCart(request.dto.UserName, cancellationToken);
+            if(basket == null)
+            {
+                return new CheckoutBasketResult(false);
+            }
+            var eventMessage = request.dto.Adapt<BasketCheckoutEvent>();
+            eventMessage.TotalPrice = basket.TotalPrice;
+            await publishEndpoint.Publish(eventMessage, cancellationToken);
+
+            await repository.DeleteShoppingCart(request.dto.UserName, cancellationToken);
+
+            return new CheckoutBasketResult(true);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,54 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+using Ordering.Application.Orders.Commands.CreateOrder;
+using Ordering.Domain.Enums;
+
+namespace Ordering.Application.Orders.EventHandlers.Integration
+{
+    public class BasketCheckoutEventHandler
+        (ISender sender, ILogger<BasketCheckoutEventHandler> logger): IConsumer<BasketCheckoutEvent>
+    {
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            logger.LogInformation("Intgration Event handled:{IntergrationEvent}", context.Message.GetType().Name);
+
+            var command = MapToCreateOrderCommand(context.Message);
+            await sender.Send(command);
+        }
+
+        public CreateOrderCommand MapToCreateOrderCommand(BasketCheckoutEvent message)
+        {
+            var addressDto = new AddressDto(message.FirstName,
+                message.LastName,
+                message.EmailAddress,
+                message.AddressLine,
+                message.Country,
+                message.State,
+                message.ZipCode);
+
+            var payment = new PaymentDto(message.CardName,
+                message.CardNumber,
+                message.Expiration,
+                message.CVV,
+                message.PaymentMethod);
+
+            var orderId = Guid.NewGuid();
+
+            var OrderDto = new OrderDto(
+                orderId,
+                message.CustomerId,
+                message.UserName,
+                addressDto,
+                addressDto,
+                payment,
+                OrderStatus.Pending,
+                [
+                    new OrderItemDto(orderId,new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61"),2,180),
+                    new OrderItemDto(orderId,new Guid("c67d6323-e8b1-4bdf-9a75-b0d0d2e7e914"),2,100),
+                    ]
+                );
+
+            return new CreateOrderCommand(OrderDto);
+        }
+    }
+}
